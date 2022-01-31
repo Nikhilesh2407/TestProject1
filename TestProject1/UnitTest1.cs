560f36916d88dc4f50a7e23e0953d6b2531128fb
USER <USER@DESKTOP-D64VQK2>
2022-01-31 16:47:29 +0530

    Add project files.

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,26 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TestProject1
+{
+    public class Tests
+    {
+        IWebDriver Driver;
+        [SetUp]
+        public void Setup()
+        {
+            Driver = new ChromeDriver();
+            Driver.Navigate().GoToUrl("https://www.arm.com/");
+            Driver.Manage().Window.Maximize();  
+        }
+
+        [Test]
+        public void Test1()
+        {
+            string Gettitle;
+            Gettitle = Driver.Title;
+            Assert.AreEqual("Artificial Intelligence Enhanced Computing – Arm®", Gettitle);
+        }
+    }
+}
